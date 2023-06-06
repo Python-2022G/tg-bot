fd2459f057da9a5e47bd3b495b890c43e0a9d75f
djumanov <diyorbekjumanov1120@gmail.com>
2023-06-06 12:02:06 +0500

    echo with package

--- bot.py
+++ bot.py
@@ -1,13 +1,20 @@
 import telegram
+import time
 
 TOKEN = '6203711973:AAGQ0KgDqOiko9KnQZOF1i8hIluAuCuBWDk'
 
 bot = telegram.Bot(token=TOKEN)
 
-# bot.send_message(chat_id='1258594598', text='hi')
+last_update_id = bot.getUpdates()[-1].update_id
+while True:
+    time.sleep(1)
 
-updates = bot.getUpdates()
+    curr_update = bot.getUpdates()[-1]
 
-last_updates = updates[-1]
+    if last_update_id != curr_update.update_id:
+        chat_id = curr_update.message.chat.id
+        text    = curr_update.message.text
 
-print(last_updates.update_id, last_updates.message.text)
+        bot.sendMessage(chat_id, text)
+
+        last_update_id = curr_update.update_id
