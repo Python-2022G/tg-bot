e84c9d941e53f74723a739b1397178c5b99fd3e4
djumanov <diyorbekjumanov1120@gmail.com>
2023-06-06 11:44:16 +0500

    get updates

--- bot.py
+++ bot.py
@@ -4,4 +4,10 @@ TOKEN = '6203711973:AAGQ0KgDqOiko9KnQZOF1i8hIluAuCuBWDk'
 
 bot = telegram.Bot(token=TOKEN)
 
-bot.send_message(chat_id='1258594598', text='hi')
+# bot.send_message(chat_id='1258594598', text='hi')
+
+updates = bot.getUpdates()
+
+last_updates = updates[-1]
+
+print(last_updates.update_id, last_updates.message.text)
